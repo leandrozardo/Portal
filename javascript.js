5f609d98a3f3f6f67367a8720495bb7cdf5448bc
Leandro A Zardo <leandro.zardo92@gmail.com>
2019-07-03 15:42:09 -0300

    Site sobre uma Escola Fictícia
    Esse é um Projeto Final da disciplina de Desenvolvimento Web na graduação de Análise e Desenvolvimento de Sistemas- 1º Semestre.
    O site se trata de uma escola fictícia, contém páginas sobre:
    Início: Contendo notícias fictícias; 
    Sobre: Falando um pouco da história;
    Disciplinas: Mostrando a grade curricular conforme a série estudada; 
    Biblioteca: Contém uma visão superficial dos livros existentes em uma biblioteca virtual existente;  
    Inscreva-se: Formulário com cadastro para receber o contato da Escola.

---
+++ javascript.js
@@ -0,0 +1,24 @@
+
+ function escondetabela(id){
+
+ 	document.getElementById(id).style.display = 'none';
+ }
+
+ function mostratabela(id){
+	//alert(id);
+	document.getElementById(id).style.display = 'block';
+
+}
+
+function escondemenu(id){
+
+ 	document.getElementById(id).style.display = 'none';
+ }
+
+function mostramenu(id){
+
+ 	document.getElementById(id).style.display = 'block';
+ }
+ function mostraAlerta() {
+	alert("Bem-Vindo ao site da Escola Yale, clique em OK para continuar!");
+}
